27f018c33d99f2b9019ee0b47ef5ed6ba51a07fb
Naman Vashishth <namanvashishth@Namans-MacBook-Air.local>
2023-05-22 10:34:30 +0530

    print statements added for understanding of execution.

--- apply_rules.py
+++ apply_rules.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     from helpers.db_operations import FetchEmails
 
     #read rules json file
+    print("Fetching rules from rules.json")
     with open("rules.json", 'r') as rules:
         rules = json.load(rules)
 
@@ -11,14 +12,22 @@ if __name__ == "__main__":
 
     #apply rules and perform action on all the eligible emails with condition "all"
     for rule in rules["all"]:
+        print(f'Fetching eligible ids for rule: {rule["name"]}')
         ids = fetch.fetch_all(rule["conditions"])
+        print(f'Fetched {len(ids)} for rule: {rule["name"]}')
         if ids:
+            print(f'Performing actions on fetched ids')
             ApplyRules(ids, rule["actions"])
+            print(f'Operation successful for rule: {rule["name"]}')
 
     #apply rules and perform action on all the eligible emails with condition "any"
     for rule in rules["any"]:
+        print(f'Fetching eligible ids for rule: {rule["name"]}')
         ids = fetch.fetch_any(rule["conditions"])
+        print(f'Fetched {len(ids)} for rule: {rule["name"]}')
         if ids:
+            print(f'Performing actions on fetched ids')
             ApplyRules(ids, rule["actions"])
+            print(f'Operation successful for rule: {rule["name"]}')
 
     print("Done")
--- fetch_mails.py
+++ fetch_mails.py
@@ -8,13 +8,19 @@ if __name__ == "__main__":
     mail_service = MailService()
 
     #get all ids of all emails
+    print("Fetching ids.")
     ids = mail_service.get_ids()
+    print(f"Fetched {len(ids)} ids.")
 
     #filter out latest 500 emails to avoid rate limiting while fetching emails
     ids = ids[:500]
 
     #raw emails
+    print(f"Fetching raw emails for first {len(ids)} ids.")
     mails = mail_service.get_mails(ids)
 
     #clean raw emails and insert in database
+    print("Inserting emails in database.")
     InsertEmails(mails=mails)
+    print("Insert operation successful.")
+
